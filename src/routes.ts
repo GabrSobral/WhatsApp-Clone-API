ffb64a17a4ba23499d9cbca28920ba5d476a3908
Sobraloser <Asgoth55@gmail.com>
2021-07-27 22:58:10 -0300

    refact: update rotues

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import MessageController from './controllers/MessageController.js'
-import userController from './controllers/userController.js'
 import RoomController from './controllers/RoomController.js'
 
 import checkAuth from './middlewares/checkAuth.js'
+import CreateUserController from './controllers/Users/CreateUserController.js'
+import ShowUserController from './controllers/Users/ShowUserController.js'
+import AuthenticateController from './controllers/Users/AuthenticateController.js'
+import DeleteUserController from './controllers/Users/DeleteUserController.js'
+import LogoutUserController from './controllers/Users/LogoutUserController.js'
+import ListUsersController from './controllers/Users/ListUsersController.js'
 
 const router = express.Router()
 
@@ -11,12 +16,12 @@ router.post('/messages/new',[checkAuth], MessageController.CreateMessage)
 router.get('/messages/sync',[checkAuth], MessageController.ListMessages)
 router.delete('/messages/delete/:id', [checkAuth], MessageController.DeleteMessages)
 
-router.post('/register', userController.CreateUser)
-router.get('/users', [checkAuth], userController.ListUsers)
-router.get('/users/:id', [checkAuth], userController.showUser)
-router.post('/authenticate', userController.Authenticate)
-router.delete('/users/:id', [checkAuth], userController.DeleteUser)
-router.patch('/users/logout', [checkAuth], userController.LogoutUser)
+router.post('/register', CreateUserController.handle)
+router.post('/authenticate', AuthenticateController.handle)
+router.get('/users', [checkAuth], ListUsersController.handle)
+router.get('/users/:id', [checkAuth], ShowUserController.handle)
+router.delete('/users/:id', [checkAuth], DeleteUserController.handle)
+router.patch('/users/logout', [checkAuth], LogoutUserController.handle)
 
 router.post('/room', [checkAuth] , RoomController.CreateRoom)
 router.get('/room', [checkAuth], RoomController.ListRooms)
