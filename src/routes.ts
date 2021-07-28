53f75dad9a7347e72a25b050424266b4546e0894
Sobraloser <Asgoth55@gmail.com>
2021-07-28 19:25:12 -0300

    feat: update routes

--- src/routes.ts
+++ src/routes.ts
@@ -13,23 +13,29 @@ import CreateMessageController from './controllers/Messages/CreateMessageControl
 import ListMessagesController from './controllers/Messages/ListMessagesController'
 import DeleteMessagesController from './controllers/Messages/DeleteMessagesController'
 
+import CreateRoomController from './controllers/Room/CreateRoomController'
+import ListRoomsController from './controllers/Room/ListRoomsController'
+import ShowRoomController from './controllers/Room/ShowRoomController'
+import ListRoomMessagesController from './controllers/Room/ListRoomMessagesController'
+import DeleteRoomController from './controllers/Room/DeleteRoomController'
+
 const router = express.Router()
 
-router.post('/messages/new',[checkAuth], CreateMessageController.handle)
-router.get('/messages/sync',[checkAuth], ListMessagesController.handle)
-router.delete('/messages/delete/:id', [checkAuth], DeleteMessagesController.handle)
+router.post('/messages/new', checkAuth, CreateMessageController.handle)
+router.get('/messages/list', checkAuth, ListMessagesController.handle)
+router.delete('/messages/delete/:id', checkAuth, DeleteMessagesController.handle)
 
 router.post('/users/register', CreateUserController.handle)
 router.post('/users/authenticate', AuthenticateController.handle)
-router.get('/users/list', [checkAuth], ListUsersController.handle)
-router.get('/users/show/:id', [checkAuth], ShowUserController.handle)
-router.delete('/users/delete/:id', [checkAuth], DeleteUserController.handle)
-router.patch('/users/logout', [checkAuth], LogoutUserController.handle)
+router.get('/users/list', checkAuth, ListUsersController.handle)
+router.get('/users/show/:id', checkAuth, ShowUserController.handle)
+router.delete('/users/delete/:id', checkAuth, DeleteUserController.handle)
+router.patch('/users/logout', checkAuth, LogoutUserController.handle)
 
-// router.post('/room', [checkAuth] , RoomController.CreateRoom)
-// router.get('/room', [checkAuth], RoomController.ListRooms)
-// router.get('/room/:id', [checkAuth], RoomController.ShowRoom)
-// router.get('/room/messages/:id', [checkAuth], RoomController.ListRoomMessages)
-// router.delete('/room/:id', [checkAuth], RoomController.DeleteRoom)
+router.post('/room/new/:id', checkAuth , CreateRoomController.handle)
+router.get('/room/list', checkAuth, ListRoomsController.handle)
+router.get('/room/:id', checkAuth, ShowRoomController.handle)
+router.get('/room/messages/list/:id', checkAuth, ListRoomMessagesController.handle)
+router.delete('/room/delete/:id', checkAuth, DeleteRoomController.handle)
 
 export default router
