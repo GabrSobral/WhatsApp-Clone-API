205bd2ccbfabf6456003f7924ff195e02c262ee2
Sobraloser <Asgoth55@gmail.com>
2021-07-29 20:36:20 -0300

    feat: update listRooms to list only my rooms

--- src/controllers/Room/ListRoomsController.ts
+++ src/controllers/Room/ListRoomsController.ts
@@ -3,7 +3,8 @@ import ListRoomsService from "../../services/Room/ListRoomsService";
 
 class ListRoomsController {
   async handle(request: Request, response: Response) {
-    const rooms = await ListRoomsService.execute();
+    const user_id = request.user_id
+    const rooms = await ListRoomsService.execute(user_id);
 
     return response.json(rooms);
   }
--- src/services/Room/ListRoomsService.ts
+++ src/services/Room/ListRoomsService.ts
@@ -1,10 +1,19 @@
 import { Room } from "../../models/Room"
+import { IUser } from "../../models/Users"
 
 class ListRoomsService{
-  async execute(){
-    const rooms = await Room.find().populate(['users', 'messages'])
+  async execute(user_id: string){
+    const rooms = 
+      await Room.find({ users: {$in:user_id} })
+      .populate(['users', 'messages'])
 
-    return rooms
+    const formattedRooms = rooms.map(room => ({
+      _id: room._id,
+      messages: room.messages,
+      user: room.users.filter((user: IUser) => String(user._id) !== String(user_id))
+    }))
+
+    return formattedRooms
   }
 }
 export default new ListRoomsService()
