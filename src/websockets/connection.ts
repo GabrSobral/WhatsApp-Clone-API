964363cd0135dde08168bb8f844ffa2fe17bf7ec
Sobraloser <Asgoth55@gmail.com>
2021-08-03 23:31:25 -0300

    feat: add viewUnreadMessages and create they

--- src/websockets/connection.ts
+++ src/websockets/connection.ts
@@ -1,12 +1,13 @@
 import { io } from "../http"
 import { Socket } from 'socket.io'
+import { UnreadMessages } from "../models/UnreadMessages"
 
 io.on('connection', (socket: Socket) => {
   
   console.log('A new user has been connected: ', socket.id)
 
   socket.on('joinroom', ({rooms}) => {
-    rooms.forEach(item => {
+    rooms.forEach((item: string) => {
       console.log(item)
      socket.join(item)
     })
@@ -15,12 +16,21 @@ io.on('connection', (socket: Socket) => {
   socket.on('leaveRoom', (room) => {
     socket.leave(room)
   })
+  socket.on('viewUnreadMessages', async({ user, room }) => {
+    console.log(user, room, "banana")
+    await UnreadMessages.deleteMany({ to: room, user: { $nin:user } })
+  })
 
   socket.on('disconnect', () =>{
     console.log('A user has been disconnected')
   })
 
-  socket.on('sendMessage', ({messageData, room})=>{
-    io.to(room).emit('newMessage', {messageData})
+  socket.on('sendMessage', async ({messageData, room}) => {
+    const unreadMessages = await UnreadMessages.create({
+      user: messageData.user,
+      message: messageData,
+      to: messageData.assignedTo
+    })
+    io.to(room).emit('newMessage', {messageData, unreadMessages})
   })
 })
