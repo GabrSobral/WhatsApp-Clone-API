833c990ec1e1707e0bd09eaa164ce54ca1232923
Sobraloser <Asgoth55@gmail.com>
2021-08-03 23:31:54 -0300

    feat: add list unreadMessages with all rooms

--- src/services/Room/ListRoomsService.ts
+++ src/services/Room/ListRoomsService.ts
@@ -1,22 +1,28 @@
 import { Room } from "../../models/Room"
+import { UnreadMessages } from "../../models/UnreadMessages"
 import { IUser } from "../../models/Users"
 
 class ListRoomsService{
   async execute(user_id: string){
     const rooms = 
-      await Room.find({ users: {$in:user_id} }, { messages: {$slice: -1} })
-      .populate(['users', 'messages'])
+      await Room.find(
+        { users: {$in:user_id} }, { messages: {$slice: -1} }
+      ).populate(['users', 'messages'])
+      
+      const formattedRooms =  await Promise.all(
+        rooms.map(async (room) => {
+          const unreadMessages = await UnreadMessages.countDocuments(
+            { to: room._id, user: { $nin:user_id } }
+          )
 
-
-    const formattedRooms= []
-
-    rooms.forEach(room => {
-      formattedRooms.push({
-        _id: room._id,
-        messages: room.messages,
-        user: room.users.filter((user: IUser) => String(user._id) !== String(user_id))
-      })
-    })
+          return({
+            _id: room._id,
+            messages: room.messages,
+            user: room.users.filter((user: IUser) => String(user._id) !== String(user_id)),
+            unreadMessages
+          })
+        })
+      )
 
     return formattedRooms
   }
