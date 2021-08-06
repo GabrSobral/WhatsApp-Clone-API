44d8e4eeca51c174d7cc6faf18b7558f3f5244ca
Sobraloser <Asgoth55@gmail.com>
2021-08-05 23:00:53 -0300

    feat: add receiveReadMessage socket

--- src/websockets/connection.ts
+++ src/websockets/connection.ts
@@ -1,22 +1,27 @@
 import { io } from "../http"
 import { Socket } from 'socket.io'
 import { UnreadMessages } from "../models/UnreadMessages"
+import { Messages } from "../models/Message"
 
 io.on('connection', (socket: Socket) => {
-  
   console.log('A new user has been connected: ', socket.id)
 
   socket.on('joinroom', ({rooms}) => {
     rooms.forEach((item: string) => {
      socket.join(item)
     })
-
   })
+
   socket.on('leaveRoom', (room) => {
     socket.leave(room)
   })
+
   socket.on('viewUnreadMessages', async({ user, room }) => {
     await UnreadMessages.deleteMany({ to: room, user: { $nin:user } })
+    await Messages.updateMany(
+      { viewed: false }, {"$set":{"viewed": true}})
+
+    io.to(user).emit('receiveReadMessages', { room })
   })
 
   socket.on('writting', ({ to, writting, room }) => {
