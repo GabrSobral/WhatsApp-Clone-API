287df60c16c886c0abe083503b50223dd107e004
Sobraloser <Asgoth55@gmail.com>
2021-08-25 16:49:42 -0300

    fix: remove console.logs

--- src/services/Status/ListStatusToMeService.ts
+++ src/services/Status/ListStatusToMeService.ts
@@ -6,12 +6,7 @@ class ListStatusToMeService {
     const statusToMe = await Status.find({ destinedTo : { $in: my_id } })
     const statusToMeUpdated = []
 
-    console.log("status para mim ",statusToMe)
-
     statusToMe.forEach(async (item: IStatusSchema) => {
-      console.log('item date:', item.validity.getTime() )
-      console.log('current date:', currentDate.getTime())
-
       if(item.validity.getTime() < currentDate.getTime()) {
         await Status.deleteOne(item._id)
         return
@@ -22,11 +17,9 @@ class ListStatusToMeService {
         owner: item.owner,
         color: item.color,
         message: item.message
-      }
-
+      } 
       statusToMeUpdated.push(itemFormatted)
     })
-    console.log(statusToMeUpdated)
     return statusToMeUpdated
   } 
 }
