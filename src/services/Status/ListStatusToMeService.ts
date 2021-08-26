868d99d5f654f790fcd4be4db33055135381b622
Sobraloser <Asgoth55@gmail.com>
2021-08-26 11:03:47 -0300

    fix: Logic in ListStatusToMeService

--- src/services/Status/ListStatusToMeService.ts
+++ src/services/Status/ListStatusToMeService.ts
@@ -1,26 +1,51 @@
 import { IStatusSchema, Status } from "../../models/Status"
+import { IUser } from "../../models/Users"
+
+interface IStatusFormat {
+  _id: String;
+  file: String;
+  color: String;
+  message: String;
+  createdAt: Date;
+}
+
+interface IStatusDataFormat {
+  owner?: IUser;
+  status?: IStatusFormat[]
+}
 
 class ListStatusToMeService {
   async execute(my_id: string){
     const currentDate = new Date()
-    const statusToMe = await Status.find({ destinedTo : { $in: my_id } })
-    const statusToMeUpdated = []
+    const usersThatHasStatus: IStatusDataFormat[] = []
+    let currentUser = ''
+    let currentIndex = 0
+    const statusToMe = 
+      await Status.find({ destinedTo : { $in: my_id } }).populate('owner')
 
-    statusToMe.forEach(async (item: IStatusSchema) => {
-      if(item.validity.getTime() < currentDate.getTime()) {
-        await Status.deleteOne(item._id)
-        return
+      statusToMe.forEach(async (item: IStatusSchema) => {
+        if(item.validity.getTime() < currentDate.getTime()) {
+          await Status.findByIdAndDelete(item._id)
+          return
       }
+
       const itemFormatted = {
         _id: item._id,
         file: item.file,
-        owner: item.owner,
         color: item.color,
-        message: item.message
-      } 
-      statusToMeUpdated.push(itemFormatted)
+        message: item.message,
+        createdAt: item.createdAt
+      }
+      
+      if(String(item.owner._id) !== String(currentUser)) {
+        currentUser = item.owner._id
+        usersThatHasStatus.push({ owner: item.owner, status: [] })
+        currentIndex = usersThatHasStatus.length - 1
+      }
+
+      usersThatHasStatus[currentIndex].status.push(itemFormatted)
     })
-    return statusToMeUpdated
+    return usersThatHasStatus
   } 
 }
 export default new ListStatusToMeService()
